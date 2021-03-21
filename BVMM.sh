3267037c3dce7e24abae91486c1a5a2a4f39119b
Rebecca <kimitxuni@gmail.com>
2021-03-21 00:30:32 +0000

    Create BVMM.sh

---
+++ BVMM.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+ISODIR="/mnt/uwu/isos"
+VMDIRECTORY="/mnt/uwu/drives"
+cd $VMDIRECTORY
+clear
+echo "Bash Virtual Machine Manager"
+echo ""
+echo "[ 1 ] Start VM"
+echo "[ 2 ] Create VM"
+echo "[ 3 ] Exit"
+echo ""
+read choice
+
+[[ -z $choice ]] && echo "Select an option"
+if [[ $choice == "1" ]]; then
+	echo ""
+	echo "Available Virtual Machines"
+	echo ""
+	ls *.sh | sed 's/.sh//g' | sed 's/BVMM//g' | sed 's/ //g'
+	echo ""
+	read vm
+	"./$vm.sh" || echo "well shite" || echo "HOW?!"
+fi
+
+if [[ $choice == "2" ]]; then
+	echo ""
+	
+	echo "Virtual Machine Name"
+	read vmname
+	
+	echo "ISO Name"
+	read isoloc
+	
+	echo "Amount of RAM (MiB)"
+	read ram
+
+	echo "# of cores"
+	read cores
+
+	echo "Save to"
+	read save
+	savefile=$save".sh"
+
+	echo "#!/bin/bash" >> $savefile
+	echo "" >> $savefile
+	echo "VMNAME=\"$vmname\"" >> $savefile
+	echo "VMLOCATION=\"$VMDIRECTORY\"" >> $savefile
+	echo "VMDRIVE=\"\$VMLOCATION/\$VMNAME.qcow2\"" >> $savefile
+	echo "CPU=\"Nehalem-v2\"" >> $savefile
+	echo "CPUFLAGS=\"-hypervisor,+vmx\"" >> $savefile
+	echo "CPUVENDOR=\"GenuineIntel\"" >> $savefile
+	echo "MACHINE=\"q35\"" >> $savefile
+	echo "ACCEL=\"kvm\"" >> $savefile
+	echo "VGA=\"qxl\"" >> $savefile
+	echo "CDROM=\"$ISODIR/$isoloc\"" >> $savefile
+	echo "USBINPUT=\"tablet\"" >> $savefile
+	echo "MEMORY=\"$ram\"" >> $savefile
+	echo "CORES=\"$cores,cores=$cores\"" >> $savefile
+	echo ""
+	echo "$(cat .template.sh)" >> $savefile
+	echo ""
+	chmod +x $savefile
+	echo "Saved"
+fi
